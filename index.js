9487fbe5b69a81050f116d4e3fb214c959e914fa
Alex Burgos <alexander.burgos1@gmail.com>
2019-12-06 12:55:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+	process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`,
+	{ useNewUrlParser: true }
+);
+
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`app running on port: ${PORT}`);
+});
