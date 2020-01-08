464ba56b3808eadf3b585510d83b434c898f2431
Alexander Burgos <alexander.burgos1@gmail.com>
2020-01-08 22:12:44 +0100

    moving to .env -wip

--- middleware/dbMiddleware.js
+++ middleware/dbMiddleware.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDb = handler => async (req, res) => {
   if (mongoose.connections[0].readyState) return handler(req, res)
   await mongoose.connect(
-    process.env.MONGODB_URI || `mongodb://localhost:27017/planning-poker`,
+    process.env.MONGODB_URI,
     {
       useNewUrlParser: true,
       useFindAndModify: false,
