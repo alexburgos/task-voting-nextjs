1e98df34f5b9be81bbe5b56ff042701131dfd14d
Alexander Burgos <alexander.burgos1@gmail.com>
2020-01-09 12:46:37 +0100

    modifying next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   env: {
-    MONGODB_URI: process.env.MONGODB_URI,
+    MONGODB_URI: process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://localhost:27017/planning-poker',
   },
 };
